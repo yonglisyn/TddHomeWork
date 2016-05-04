a2aea26f4bee40f0473bd64bcf165859cdb0c204
yonglisyn <sylyongli@gmail.com>
2016-05-04 23:07:26 +0800

    mvoe class

--- PotterShoppingCart.Tests/Models/ShoppingCart.cs
+++ PotterShoppingCart.Tests/Models/ShoppingCart.cs
@@ -1,12 +1,20 @@
-﻿namespace PotterShoppingCart.Tests.Models
+﻿using System.Collections.Generic;
+
+namespace PotterShoppingCart.Tests.Models
 {
     public class ShoppingCart
     {
         private Potter _Book;
+        private List<Potter> _Books;
 
         public ShoppingCart(Potter book)
         {
             _Book = book;
+        }
+
+        public ShoppingCart(List<Potter> books)
+        {
+            _Books = books;
         }
 
         public int GetPrice()
--- PotterShoppingCart.Tests/PotterShoppingCartTest.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PotterShoppingCart.Tests.Models;
 
 namespace PotterShoppingCart.Tests
@@ -13,6 +14,16 @@ namespace PotterShoppingCart.Tests
             var book = new Potter(version);
             var shopCart = new ShoppingCart(book);
             var expected = 100;
+            var actualPrice = shopCart.GetPrice();
+            Assert.AreEqual(expected,actualPrice);
+        }
+
+        [TestMethod]
+        public void Test_CalculatePrice_Buy_1_Book1_1_Book2_Should_Be_190()
+        {
+            var books = new List<Potter>{new Potter(1), new Potter(2)};
+            var shopCart = new ShoppingCart(books);
+            var expected = 190;
             var actualPrice = shopCart.GetPrice();
             Assert.AreEqual(expected,actualPrice);
         }
